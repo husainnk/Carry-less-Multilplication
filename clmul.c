d3bb1035ff3d0c4ec4c15303ee84f141826a008d
Mohamed Husain <nkhusain@vt.edu>
2022-12-14 17:41:41 -0500

    update clmul.c

--- clmul.c
+++ clmul.c
@@ -2,33 +2,44 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include "hpm.h"
 
 
-#define HW_CLMUL 1
 
-uint64_t  HW_CLMUL(uint32_t op_a,uint32_t op_b)
+uint64_t  HW_CLMUL(uint64_t op_a,uint64_t op_b)
 {
 
-	uint64_t result;
+	uint64_t result=0;
 	asm volatile("clmul %0, %1,%2\n":"=r"(result):"r"(op_a),"r"(op_b):);
+	printf("CLMUL:(h/w) 0x%"PRIx64 "\n",result);
 
-	printf("CLMUL:(h/w) 0x%x\n",result);
+	result=0;
+	asm volatile("clmulh %0, %1,%2\n":"=r"(result):"r"(op_a),"r"(op_b):);
+	printf("CLMUL-H:(h/w) 0x%"PRIx64 "\n",result);
+
+	result=0;
+	asm volatile("clmulbig %0, %1,%2\n":"=r"(result):"r"(op_a),"r"(op_b):);
+	printf("CLMUL-64:(h/w) 0x%"PRIx64 "\n",result);
+
+	result=0;
+	asm volatile("clmulbigh %0, %1,%2\n":"=r"(result):"r"(op_a),"r"(op_b):);
+	printf("CLMUL-64-H:(h/w) 0x%"PRIx64 "\n",result);
+
 	return result ;
 }
 
-uint64_t  SW_CLMUL(uint32_t op_a,uint32_t op_b)
+uint64_t  SW_CLMUL(uint64_t op_a,uint64_t op_b)
 {
 	uint64_t result = 0;
-	for( size_t i = 0; i < 16; ++i )
+	for( size_t i = 0; i < 32; ++i )
 	{
 		if( (op_a >> i) & 1 )
 		{
 			result ^= (op_b) << i;
 		}
 	}	
-	printf("CLMUL:(s/w) 0x%x\n",result);
-
+	printf("CLMUL:(s/w) 0x%"PRIx64 "\n",result);
 	return result ;
 }
 
@@ -38,12 +49,13 @@ int main(void)
 	hpm_init();
 
 	uint64_t ret ;
-	for(int i=0; i< 1000 ; i++)
-#ifdef HWCLMUL
-		ret = HW_CLMUL(0xffabdfdf,0xffabefef);
-#else
-		ret = SW_CLMUL(0xAD,0x10);
-#endif
+	for(int i=0; i< 1 ; i++)
+	{
+		uint64_t op_a = 0xbf4d931e ;
+		uint64_t op_b = 0x9b9c9322 ;
+		ret = HW_CLMUL(op_a,op_b);
+		ret = SW_CLMUL(op_a,op_b);
+	}
 	hpm_print();
 	return 0;
 }
